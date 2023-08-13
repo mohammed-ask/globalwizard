deadd1c86768030d673d15618da37a45e0f0b1dd
akshatgroup <marketing.akshatgroup@gmail.com>
2023-08-14 02:13:39 +0530

    changes in user dashboard

--- main/transactions.php
+++ main/transactions.php
@@ -14,171 +14,175 @@ include "main/session.php";
 </div>
 <!-- Page body -->
 <div class="page-body">
-    <div class="container-xl">
-        <div class="card">
-            <div class="row g-0">
-                <div class="col-12 col-md-3 border-end">
-                    <div class="card-body">
-                        <h4 class="subheader">Account settings</h4>
-                        <div class="list-group list-group-transparent">
-                            <a href="account" class="list-group-item list-group-item-action d-flex align-items-center">My Account</a>
+                <div class="container-xl">
+                    <div class="card">
+                        <div class="row g-0">
+                            <div class="col-12 col-md-3 border-end">
+                                <div class="card-body">
+                                    <h4 class="subheader">Account settings</h4>
+                                    <div class="list-group list-group-transparent">
+                                        <a href="/account-setting.html"
+                                            class="list-group-item list-group-item-action d-flex align-items-center">My Account</a>
 
-                            <a href="plan" class="list-group-item list-group-item-action d-flex align-items-center ">Active Plan</a>
-                            <a href="transaction" class="list-group-item list-group-item-action d-flex align-items-center active">Recent Transactions</a>
+                                        <a href="/setting-plan.html"
+                                            class="list-group-item list-group-item-action d-flex align-items-center ">Active Plan</a>
+                                        <a href="/transection.html"
+                                            class="list-group-item list-group-item-action d-flex align-items-center active">Recent Transactions</a>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-9 d-flex flex-column">
+
+                                <div class="col-12">
+                                    <div class="card">
+                                      <div class="card-header">
+                                        <h3 class="card-title">Recent Transactions</h3>
+                                      </div>
+                                      
+                                      <div class="table-responsive fixTableHead" style="height: 400px;">
+                                        <table class="table card-table table-vcenter text-nowrap datatable">
+                                          <thead>
+                                            <tr>
+                                              <th>Date</th>
+                                              <th>Time</th>
+                                              <th>Transaction Id</th>
+                                              <th>Payment Mode</th>
+                                              <th>Amount</th>
+                                              <th>Status</th>
+                                            </tr>
+                                          </thead>
+                                          <tbody>
+                                            <tr>
+                                              <td>23 Jun, 2023</td>
+                                              <td>16:23</td>
+                                              <td>TR6377366678388</td>
+                                              <td>Upi Transfer</td>
+                                              <td><span>₹</span>88798</td>
+                                              <td><span class="badge bg-success me-1"></span>Success</td>
+                                              
+                                            </tr>
+                                            <tr>
+                                             
+                                                <td>23 Jun, 2023</td>
+                                                <td>16:23</td>
+                                                <td>TR6377366678388</td>
+                                                <td>Upi Transfer</td>
+                                                <td><span>₹</span>8889077</td>
+                                              <td><span class="badge bg-warning me-1"></span>Pending</td>
+                                            </tr>
+                                           
+                                            <tr>
+                                                <td>23 Jun, 2023</td>
+                                                <td>16:23</td>
+                                                <td>TR6377366678388</td>
+                                                <td>Upi Transfer</td>
+                                                <td><span>₹</span>88798</td>
+                                                <td><span class="badge bg-success me-1"></span>Success</td>
+                                                
+                                              </tr>
+                                              <tr>
+                                               
+                                                  <td>23 Jun, 2023</td>
+                                                  <td>16:23</td>
+                                                  <td>TR6377366678388</td>
+                                                  <td>Upi Transfer</td>
+                                                  <td><span>₹</span>8889077</td>
+                                                <td><span class="badge bg-warning me-1"></span>Pending</td>
+                                              </tr>
+
+                                              <tr>
+                                                <td>23 Jun, 2023</td>
+                                                <td>16:23</td>
+                                                <td>TR6377366678388</td>
+                                                <td>Upi Transfer</td>
+                                                <td><span>₹</span>88798</td>
+                                                <td><span class="badge bg-success me-1"></span>Success</td>
+                                                
+                                              </tr>
+                                              <tr>
+                                               
+                                                  <td>23 Jun, 2023</td>
+                                                  <td>16:23</td>
+                                                  <td>TR6377366678388</td>
+                                                  <td>Upi Transfer</td>
+                                                  <td><span>₹</span>8889077</td>
+                                                <td><span class="badge bg-warning me-1"></span>Pending</td>
+                                              </tr>
+
+                                              <tr>
+                                                <td>23 Jun, 2023</td>
+                                                <td>16:23</td>
+                                                <td>TR6377366678388</td>
+                                                <td>Upi Transfer</td>
+                                                <td><span>₹</span>88798</td>
+                                                <td><span class="badge bg-success me-1"></span>Success</td>
+                                                
+                                              </tr>
+                                              <tr>
+                                               
+                                                  <td>23 Jun, 2023</td>
+                                                  <td>16:23</td>
+                                                  <td>TR6377366678388</td>
+                                                  <td>Upi Transfer</td>
+                                                  <td><span>₹</span>8889077</td>
+                                                <td><span class="badge bg-warning me-1"></span>Pending</td>
+                                              </tr>
+
+                                              <tr>
+                                                <td>23 Jun, 2023</td>
+                                                <td>16:23</td>
+                                                <td>TR6377366678388</td>
+                                                <td>Upi Transfer</td>
+                                                <td><span>₹</span>88798</td>
+                                                <td><span class="badge bg-success me-1"></span>Success</td>
+                                                
+                                              </tr>
+                                              <tr>
+                                               
+                                                  <td>23 Jun, 2023</td>
+                                                  <td>16:23</td>
+                                                  <td>TR6377366678388</td>
+                                                  <td>Upi Transfer</td>
+                                                  <td><span>₹</span>8889077</td>
+                                                <td><span class="badge bg-warning me-1"></span>Pending</td>
+                                              </tr>
+
+                                              <tr>
+                                                <td>23 Jun, 2023</td>
+                                                <td>16:23</td>
+                                                <td>TR6377366678388</td>
+                                                <td>Upi Transfer</td>
+                                                <td><span>₹</span>88798</td>
+                                                <td><span class="badge bg-success me-1"></span>Success</td>
+                                                
+                                              </tr>
+                                              <tr>
+                                               
+                                                  <td>23 Jun, 2023</td>
+                                                  <td>16:23</td>
+                                                  <td>TR6377366678388</td>
+                                                  <td>Upi Transfer</td>
+                                                  <td><span>₹</span>8889077</td>
+                                                <td><span class="badge bg-warning me-1"></span>Pending</td>
+                                              </tr>
+                                           
+                                          </tbody>
+                                        </table>
+                                      </div>
+                                      
+                                    </div>
+                                  </div>
+
+
+
+
+                            </div>
+                            
                         </div>
                     </div>
                 </div>
-                <div class="col-12 col-md-9 d-flex flex-column" style="height: 400px; overflow-y: scroll;">
-
-                    <div class="col-12">
-                        <div class="card">
-                            <div class="card-header">
-                                <h3 class="card-title">Recent Transactions</h3>
-                            </div>
-
-                            <div class="table-responsive">
-                                <table class="table card-table table-vcenter text-nowrap datatable">
-                                    <thead>
-                                        <tr>
-                                            <th>Date</th>
-                                            <th>Time</th>
-                                            <th>Transaction Id</th>
-                                            <th>Payment Mode</th>
-                                            <th>Amount</th>
-                                            <th>Status</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>23 Jun, 2023</td>
-                                            <td>16:23</td>
-                                            <td>TR6377366678388</td>
-                                            <td>Upi Transfer</td>
-                                            <td><span>₹</span>88798</td>
-                                            <td><span class="badge bg-success me-1"></span>Success</td>
-
-                                        </tr>
-                                        <tr>
-
-                                            <td>23 Jun, 2023</td>
-                                            <td>16:23</td>
-                                            <td>TR6377366678388</td>
-                                            <td>Upi Transfer</td>
-                                            <td><span>₹</span>8889077</td>
-                                            <td><span class="badge bg-warning me-1"></span>Pending</td>
-                                        </tr>
-
-                                        <tr>
-                                            <td>23 Jun, 2023</td>
-                                            <td>16:23</td>
-                                            <td>TR6377366678388</td>
-                                            <td>Upi Transfer</td>
-                                            <td><span>₹</span>88798</td>
-                                            <td><span class="badge bg-success me-1"></span>Success</td>
-
-                                        </tr>
-                                        <tr>
-
-                                            <td>23 Jun, 2023</td>
-                                            <td>16:23</td>
-                                            <td>TR6377366678388</td>
-                                            <td>Upi Transfer</td>
-                                            <td><span>₹</span>8889077</td>
-                                            <td><span class="badge bg-warning me-1"></span>Pending</td>
-                                        </tr>
-
-                                        <tr>
-                                            <td>23 Jun, 2023</td>
-                                            <td>16:23</td>
-                                            <td>TR6377366678388</td>
-                                            <td>Upi Transfer</td>
-                                            <td><span>₹</span>88798</td>
-                                            <td><span class="badge bg-success me-1"></span>Success</td>
-
-                                        </tr>
-                                        <tr>
-
-                                            <td>23 Jun, 2023</td>
-                                            <td>16:23</td>
-                                            <td>TR6377366678388</td>
-                                            <td>Upi Transfer</td>
-                                            <td><span>₹</span>8889077</td>
-                                            <td><span class="badge bg-warning me-1"></span>Pending</td>
-                                        </tr>
-
-                                        <tr>
-                                            <td>23 Jun, 2023</td>
-                                            <td>16:23</td>
-                                            <td>TR6377366678388</td>
-                                            <td>Upi Transfer</td>
-                                            <td><span>₹</span>88798</td>
-                                            <td><span class="badge bg-success me-1"></span>Success</td>
-
-                                        </tr>
-                                        <tr>
-
-                                            <td>23 Jun, 2023</td>
-                                            <td>16:23</td>
-                                            <td>TR6377366678388</td>
-                                            <td>Upi Transfer</td>
-                                            <td><span>₹</span>8889077</td>
-                                            <td><span class="badge bg-warning me-1"></span>Pending</td>
-                                        </tr>
-
-                                        <tr>
-                                            <td>23 Jun, 2023</td>
-                                            <td>16:23</td>
-                                            <td>TR6377366678388</td>
-                                            <td>Upi Transfer</td>
-                                            <td><span>₹</span>88798</td>
-                                            <td><span class="badge bg-success me-1"></span>Success</td>
-
-                                        </tr>
-                                        <tr>
-
-                                            <td>23 Jun, 2023</td>
-                                            <td>16:23</td>
-                                            <td>TR6377366678388</td>
-                                            <td>Upi Transfer</td>
-                                            <td><span>₹</span>8889077</td>
-                                            <td><span class="badge bg-warning me-1"></span>Pending</td>
-                                        </tr>
-
-                                        <tr>
-                                            <td>23 Jun, 2023</td>
-                                            <td>16:23</td>
-                                            <td>TR6377366678388</td>
-                                            <td>Upi Transfer</td>
-                                            <td><span>₹</span>88798</td>
-                                            <td><span class="badge bg-success me-1"></span>Success</td>
-
-                                        </tr>
-                                        <tr>
-
-                                            <td>23 Jun, 2023</td>
-                                            <td>16:23</td>
-                                            <td>TR6377366678388</td>
-                                            <td>Upi Transfer</td>
-                                            <td><span>₹</span>8889077</td>
-                                            <td><span class="badge bg-warning me-1"></span>Pending</td>
-                                        </tr>
-
-                                    </tbody>
-                                </table>
-                            </div>
-
-                        </div>
-                    </div>
-
-
-
-
-                </div>
-
             </div>
-        </div>
-    </div>
-</div>
 
 <?php
 $pagemaincontent = ob_get_contents();
