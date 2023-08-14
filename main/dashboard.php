472c5bd82ed50b897611f5b190fee7d4103a599d
akshatgroup <marketing.akshatgroup@gmail.com>
2023-08-14 18:25:16 +0530

    success model and danger model Added

--- main/dashboard.php
+++ main/dashboard.php
@@ -386,12 +386,97 @@ include "main/session.php";
                             </div>
                             <div class="modal-footer">
                               <button type="button" class="btn me-auto" data-bs-dismiss="modal">Close</button>
-                              <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Send Payment Details For Approval</button>
+                              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal-success">Send Payment Details For Approval</button>
+                              <!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal-danger">Send Payment Details For Approval</button> -->
+
                             </div>
                           </div>
                         </div>
                       </div>
 
+                      <!-- --------------------------success model Start--------- -->
+                      <div class="modal modal-blur fade" id="modal-success" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog modal-sm modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <div class="modal-status bg-success"></div>
+          <div class="modal-body text-center py-4">
+           
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon mb-2 text-green icon-lg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path><path d="M9 12l2 2l4 -4"></path></svg>
+            <h3>Payment succedeed</h3>
+            <div class="">Your payment of ₹ 2897690 has been successfully submitted. Your Membership will Active once your payment will Approved</div>
+          </div>
+          <div class="modal-footer">
+            <div class="w-100">
+              <div class="row">
+                <div class="col"><a href="#" class="btn w-100" data-bs-dismiss="modal">
+                    Go to dashboard
+                  </a></div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="modal modal-blur fade" id="modal-danger" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog modal-sm modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <div class="modal-status bg-danger"></div>
+          <div class="modal-body text-center py-4">
+            <!-- Download SVG icon from http://tabler-icons.io/i/alert-triangle -->
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon mb-2 text-danger icon-lg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z"></path><path d="M12 9v4"></path><path d="M12 17h.01"></path></svg>
+            <h3>Are you sure?</h3>
+            <div class="text-secondary">Do you really want to remove 84 files? What you've done cannot be undone.</div>
+          </div>
+          <div class="modal-footer">
+            <div class="w-100">
+              <div class="row">
+                <div class="col"><a href="https://preview.tabler.io/modals.html#" class="btn w-100" data-bs-dismiss="modal">
+                    Cancel
+                  </a></div>
+                <div class="col"><a href="https://preview.tabler.io/modals.html#" class="btn btn-danger w-100" data-bs-dismiss="modal">
+                    Delete 84 items
+                  </a></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+                      <!-- ---------------------------------Success Model End -->
+
+<!-- ---------------------------------------danger Model Start--------------- -->
+
+<div class="modal modal-blur fade" id="modal-danger" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog modal-sm modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <div class="modal-status bg-danger"></div>
+          <div class="modal-body text-center py-4">
+
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon mb-2 text-danger icon-lg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z"></path><path d="M12 9v4"></path><path d="M12 17h.01"></path></svg>
+            <h3>Are you sure?</h3>
+            <div class="text-secondary">Do you really want? What you've done cannot be undone.</div>
+          </div>
+          <div class="modal-footer">
+            <div class="w-100">
+              <div class="row">
+                <div class="col"><a href="#" class="btn w-100" data-bs-dismiss="modal">
+                    Cancel
+                  </a></div>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+<!-- ----------------------------------------danger model end------------------ -->
 
                        <!-- ----------------------model section end -->
 
@@ -743,7 +828,6 @@ include "main/session.php";
               </div>
             </div>
 
-            
           </div>
         </div>
 <?php
