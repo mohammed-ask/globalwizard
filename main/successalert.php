5146a58d79c1d5b59e684f603183bedd7c65a238
akshatgroup <marketing.akshatgroup@gmail.com>
2023-08-25 01:19:10 +0530

    Create successalert.php

---
+++ main/successalert.php
@@ -0,0 +1,10 @@
+<div class="modal-status bg-success"></div>
+<div class="modal-body text-center py-4">
+
+<svg xmlns="http://www.w3.org/2000/svg" class="icon mb-2 text-green icon-lg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path><path d="M9 12l2 2l4 -4"></path></svg>
+    <h3>Payment Detials Sent Succussfully</h3>
+    <div class="">Payment details sent succussfully for verification,<span style="font-weight: 600; color:green;"> Once Payment Verification done by Our Team Your Plan will be Activated Soon!</span>
+</div>
+<script>
+    $('.modal-footer').html('')
+</script>
