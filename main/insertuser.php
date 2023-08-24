53183442181f5f9d9c4ee79b6b4e61ef629292bd
md ask <mohammedmaheswer12@gmail.com>
2023-08-25 00:47:40 +0530

    Update insertuser.php

--- main/insertuser.php
+++ main/insertuser.php
@@ -2,13 +2,12 @@
 session_start();
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-print_r(implode(",", $_POST['otp']));
-print_r($_SESSION['otp']);
+// print_r($_SESSION['otp']);
 // print_r($_FILES);
 // die;
 include './function.php';
 include './conn.php';
-if ($_SESSION['otp'] != implode(",", $_POST['otp'])) {
+if ($_SESSION['otp'] != implode("", $_POST['otp'])) {
     echo "Failed";
 } else {
 
